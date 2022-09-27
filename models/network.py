47c2d580972298a748014ea9fbe4e19f9219624c
Entarudin <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>
2022-09-27 19:59:57 +0300

    added fields in network

--- models/network.py
+++ models/network.py
@@ -6,3 +6,6 @@ class Network:
     def __init__(self):
         self.udp_hosts: Optional[Hosts] = None
         self.tcp_hosts: Optional[Hosts] = None
+        self.ip_hosts: Optional[Hosts] = None
+        self.current_ip_address: Optional[str] = None
+        self.current_networks_interface: Optional[str] = None
--- models/target.py
+++ models/target.py
@@ -4,6 +4,7 @@ from typing import Optional
 class Target:
     def __init__(self):
         self.ip_address: Optional[str] = None
+        self.mac_address: Optional[str] = None
         self.port_id: Optional[str] = None
         self.status: Optional[str] = None
         self.service: Optional[str] = None
