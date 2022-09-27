d385f65feeea356452e63e704b70bb3fc89fa9de
Entarudin <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>
2022-09-27 20:05:42 +0300

    added method cast arp table in target service

--- services/target_service.py
+++ services/target_service.py
@@ -1,10 +1,11 @@
-from models import Target, Host
+from models import Target, Host, Arp
 
 
 class TargetService:
     def create_target(
             self,
             ip_address: str,
+            mac_address: str,
             port_id: str,
             status: str,
             service: str,
@@ -12,11 +13,26 @@ class TargetService:
     ):
         target = Target()
         target.ip_address = ip_address
+        target.mac_address = mac_address
         target.port_id = port_id
         target.status = status
         target.service = service
         target.protocol = protocol
         return target
+
+    def cast_arp_table_in_targets(self, arp_table: list[Arp]) -> list[Target]:
+        result = []
+        for item in arp_table:
+            target = self.create_target(
+                item.ip_address,
+                item.mac_address,
+                port_id=None,
+                status=None,
+                service=None,
+                protocol='arp'
+            )
+            result.append(target)
+        return result
 
     def cast_hosts_in_targets(self, hosts: list[Host]) -> list[Target]:
         result = []
@@ -24,6 +40,7 @@ class TargetService:
             for port in item.ports:
                 target = self.create_target(
                     item.ip_address,
+                    item.mac_address,
                     port.port_id,
                     port.status,
                     port.service,
@@ -31,3 +48,4 @@ class TargetService:
                 )
                 result.append(target)
         return result
+
