7d1ee2eca6ae98110aeb51373a527d62f1f632e6
Entarudin <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>
2022-09-30 20:23:32 +0300

    rename method invoke

--- services/invoker_shell_command_service.py
+++ services/invoker_shell_command_service.py
@@ -2,11 +2,11 @@ from shell_commands_executor import ShellCommandsExecutor
 
 
 class InvokerShellCommandService:
-    def execute_one_command(self, shell_command: str) -> str:
+    def invoke_one_command(self, shell_command: str) -> str:
         result_command = ShellCommandsExecutor(shell_command).execute()
         return result_command
 
-    def execute_commands(self, shell_commands: list[str]) -> list[str]:
+    def invoke_commands(self, shell_commands: list[str]) -> list[str]:
         result_shell_commands = []
         for commands in shell_commands:
             result_command = ShellCommandsExecutor(commands).execute()
