8494f5c4e52bd286f14f5e2b04a342560acc339f
Entarudin <riazanov@sfedu.ru>
2023-01-26 14:21:46 +0300

    fix

--- translators/network_translator.py
+++ translators/network_translator.py
@@ -10,12 +10,12 @@ class NetworkTranslator:
         network = Network()
         network.tcp_hosts = self.__get_hosts(json, "tcp_ports")
         network.udp_hosts = self.__get_hosts(json, "udp_ports")
-        network.ip_hosts = self.__get_hosts(json, "protocols")
+        network.ip_hosts = self.__get_hosts(json, "ip_ports")
         network.current_ip_address = self.__get_key(json, "ip")
         network.current_networks_interface = self.__get_key(json, "current_network_interface")
         return network
 
-    def __get_hosts(self, json, key: Literal["tcp_ports", "udp_ports", "protocols"]):
+    def __get_hosts(self, json, key: Literal["tcp_ports", "udp_ports", "ip_ports"]):
         hosts_json = json.get(f"{key}", {})\
             .get("nmaprun", {})\
             .get("host", [])
